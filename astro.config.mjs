a9f2c548151e059faf426046a3e82ba7b99dfb7b
Taewan Park <swimingtw@gmail.com>
2025-04-05 15:19:18 +0900

    Format code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,36 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import cloudflare from '@astrojs/cloudflare';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import cloudflare from "@astrojs/cloudflare";
+import starlightThemeRapide from "starlight-theme-rapide"; // https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-	adapter: cloudflare({
-		platformProxy: {
-		  enabled: true
-		}
-	  }),
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
+  integrations: [
+    starlight({
+      plugins: [starlightThemeRapide()],
+      title: "My Docs",
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
