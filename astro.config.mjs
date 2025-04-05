152edd0bab5945ccdc30db6629de53aa0aa536db
Taewan Park <swimingtw@gmail.com>
2025-04-06 02:31:12 +0900

    Optimize assets & page

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,10 +16,22 @@ export default defineConfig({
     starlight({
       plugins: [starlightThemeRapide()],
       title: "리크루트 홀딩스 x KOREC 채용공고",
+      defaultLocale: "ko",
       social: {
         github: "https://github.com/Taewan-P/recruit-holdings-kr-recruit-page",
       },
       customCss: ["./src/styles/custom.css"],
+      head: [
+        {
+          tag: "link",
+          attrs: {
+            rel: "stylesheet",
+            as: "style",
+            crossOrigin: "anonymous",
+            href: "https://unpkg.com/pretendard@1.3.9/dist/web/static/pretendard-dynamic-subset.css",
+          },
+        },
+      ],
       pagefind: false,
     }),
     compressor(),
