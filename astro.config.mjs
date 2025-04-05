6b06e1823b7ee92e2f47514094617413cbeca343
Taewan Park <swimingtw@gmail.com>
2025-04-05 17:03:13 +0900

    Add draft posting data

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import cloudflare from "@astrojs/cloudflare";
-import starlightThemeRapide from "starlight-theme-rapide"; // https://astro.build/config
+import starlightThemeRapide from "starlight-theme-rapide";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,23 +13,11 @@ export default defineConfig({
   integrations: [
     starlight({
       plugins: [starlightThemeRapide()],
-      title: "My Docs",
+      title: "리크루트 홀딩스 x KOREC 채용공고",
       social: {
-        github: "https://github.com/withastro/starlight",
+        github: "https://github.com/Taewan-P/recruit-holdings-kr-recruit-page",
       },
-      sidebar: [
-        {
-          label: "Guides",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Example Guide", slug: "guides/example" },
-          ],
-        },
-        {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
-        },
-      ],
+      pagefind: false,
     }),
   ],
 });
